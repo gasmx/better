4bd0e1684513f41c8a6022078483490d865444cd
Gabriel Angelus <gangeluscf@gmail.com>
2017-10-20 21:31:45 -0200

    Stable version

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 (function( doc ) {
-  var running = false
   var notification = new Audio('https://notificationsounds.com/soundfiles/07563a3fe3bbe7e3ba84431ad9d055af/file-sounds-946-capisci.mp3');
+  console.log('Now you are betting better, created by Gabriel Angelus')
+
+  let defaultValue = 7
+
+  $('csgr-header section .nav-underlined').eq(1).prepend('<li style="margin-right: 10px" class="nav-item"><input style="width: 60px" class="form-control" id="train-length" value="'+ defaultValue +'"></li>')
 
   setInterval(function() {
-    if (running) {
-      return
+    let balance = $('csgr-roulette-wager').find('input').val()
+    let isTrain = true
+    let trainLength = $("#train-length").val()
+
+    for (let i=0; i<(trainLength-1); i++) {
+      if ($('.round').eq(i).attr('class') != $('.round').eq(i+1).attr('class')) {
+        isTrain = false
+        break;
+      }
     }
 
-    let balance = $('csgr-roulette-wager').find('input').val()
+    if (isTrain) {
+      console.log(`It's a ${$('.round').eq(0).attr('class').split(' ')[1]} one dude!`)
+      notification.play()
+    }
 
-    if (
+    /*if (
       $('.round').eq(0).hasClass('red') &&
       $('.round').eq(1).hasClass('red') &&
       $('.round').eq(2).hasClass('red') &&
@@ -29,7 +43,7 @@
     ) {
       console.log('BLACK TRAIN!')
       notification.play()
-    }
+    }*/
 
     //$("csgr-roulette-wager").find('button').eq(2).click()
     //$("csgr-scroll").find('.btn-3d-success').click()
